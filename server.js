c7c24b0a5b394ee5fa704cf6cc7ee5c92b5b9c49
Aon Mao <96533871+AonMao888@users.noreply.github.com>
2025-07-23 13:11:58 +0630

    Add files via upload

--- server.js
+++ server.js
@@ -101,6 +101,48 @@ app.get('/shop', async (req, res) => {
     }
 })
 
+//update shop data
+app.post('/update/shop', async (req, res) => {
+    let data = req.body;
+    if (data.uid) {
+        let t = await db.collection('shop').where('shopid', '==', data.currentshopid).get();
+        if (!t.empty) {
+            let gotshop = t.docs[0].data();
+            if (gotshop.ownerid === data.uid) {
+                await t.docs[0].ref.update({
+                    shopname: data.shopname,
+                    logo: data.logo,
+                    shoptype: data.shoptype,
+                    shopdes: data.shopdes,
+                    city: data.city,
+                    email: data.email,
+                    shopid: data.shopid
+                }).then(() => {
+                    res.json({
+                        status: 'success',
+                        text: 'Shop data was updated!'
+                    })
+                })
+            } else {
+                res.json({
+                    status: 'fail',
+                    text: 'You have no permission to update data!'
+                })
+            }
+        } else {
+            res.json({
+                status: 'fail',
+                text: 'This shop is not exists!'
+            })
+        }
+    } else {
+        res.json({
+            status: 'fail',
+            text: 'Unable to update shop data!'
+        })
+    }
+})
+
 app.get('/shopbyemail', async (req, res) => {
     let { email } = req.query;
     if (email) {
@@ -273,31 +315,34 @@ app.post('/post/delete', async (req, res) => {
 app.post('/contact/add', async (req, res) => {
     let data = req.body;
     if (data) {
-        let d = await db.collection('shop').doc(data.shopid).get();
-        if (d.exists) {
-            let shopdata = d.data();
+        let d = await db.collection('shop').where('shopid', '==', data.shopid).get();
+        if (!d.empty) {
+            let shopdata = d.docs[0].data();
             if (shopdata.ownerid == data.posterid) {
-                let addref = db.collection('shop').doc(data.shopid);
                 try {
-                    await addref.update({
-                        [`contact.${data.contacttype}`]: data.new
-                    }).then(() => {
-                        if (data.isupdate === true) {
-                            res.json({
-                                status: 'success',
-                                text: 'Data was updated.'
-                            });
-                        } else {
-                            res.json({
-                                status: 'success',
-                                text: 'Data was added.'
-                            });
-                        }
-                    })
+                    let addref = db.collection('shop').where('shopid', '==', data.shopid).get().then(async (querysnap) => {
+                        let doc = querysnap.docs[0];
+                        await doc.ref.update({
+                            [`contact.${data.contacttype}`]: data.new
+                        }).then(() => {
+                            if (data.isupdate === true) {
+                                res.json({
+                                    status: 'success',
+                                    text: 'Data was updated.'
+                                });
+                            } else {
+                                res.json({
+                                    status: 'success',
+                                    text: 'Data was added.'
+                                });
+                            }
+                        })
+                    });
+
                 } catch (e) {
                     res.json({
                         status: 'fail',
-                        text: 'No permission to add data!'
+                        text: 'Unable to add data!'
                     })
                 }
             } else {
@@ -324,11 +369,11 @@ app.post('/contact/add', async (req, res) => {
 app.post('/contact/addanother', async (req, res) => {
     let data = req.body;
     if (data) {
-        let d = await db.collection('shop').doc(data.shopid).get();
-        if (d.exists) {
-            let shopdata = d.data();
+        let d = await db.collection('shop').where('shopid', '==', data.shopid).get();
+        if (!d.empty) {
+            let shopdata = d.docs[0].data();
             if (shopdata.ownerid == data.posterid) {
-                let addref = db.collection('shop').doc(data.shopid);
+                let addref = d.docs[0].ref
                 try {
                     await addref.update({
                         [`anothercontact.${data.appname}`]: {
@@ -378,11 +423,11 @@ app.post('/contact/addanother', async (req, res) => {
 //delete another account
 app.post('/delanotheraccount', async (req, res) => {
     let data = req.body;
-    let d = await db.collection('shop').doc(data.shopid).get();
-    let da = d.data();
+    let d = await db.collection('shop').where('shopid', '==', data.shopid).get();
+    let da = d.docs[0].data();
     if (data.posterid === da.ownerid) {
         let path = `anothercontact.${data.appname}`;
-        await db.collection('shop').doc(data.shopid).update({
+        await d.docs[0].ref.update({
             [path]: admin.firestore.FieldValue.delete()
         }).then(() => {
             res.json({
@@ -402,10 +447,10 @@ app.post('/delanotheraccount', async (req, res) => {
 app.post('/addlocation', async (req, res) => {
     let data = req.body;
     if (data) {
-        let d = await db.collection('shop').doc(data.shopid).get();
-        let da = d.data();
+        let d = await db.collection('shop').where('shopid', '==', data.shopid).get();
+        let da = d.docs[0].data();
         if (data.posterid === da.ownerid) {
-            await db.collection('shop').doc(data.shopid).update({
+            await d.docs[0].ref.update({
                 lat: data.lat,
                 long: data.long
             }).then(() => {
@@ -432,9 +477,9 @@ app.post('/addlocation', async (req, res) => {
 app.post('/add/review', async (req, res) => {
     let data = req.body;
     if (data) {
-        let shopdata = await db.collection('shop').doc(data.shopid).get();
-        if (shopdata.exists) {
-            let da = shopdata.data();
+        let shopdata = await db.collection('shop').where('shopid', '==', data.shopid).get();
+        if (!shopdata.empty) {
+            let da = shopdata.docs[0].data();
             if (da.shopname === data.shopname) {
                 let date = new Date();
                 let hour = date.getHours();
@@ -443,18 +488,20 @@ app.post('/add/review', async (req, res) => {
                 let month = date.getMonth();
                 let year = date.getFullYear();
                 let time = hour + ':' + min + '/' + day + '.' + month + '.' + year;
-                await db.collection('reviews').doc(data.shopid).update({
-                    [data.posterid]: {
-                        text: data.text,
-                        posterid: data.posterid,
-                        postername: data.postername,
-                        posterimg: data.posterimg,
-                        posteremail: data.posteremail,
-                        shopname: data.shopname,
-                        shopid: data.shopid,
-                        time: time,
-                        fulltime: admin.firestore.FieldValue.serverTimestamp()
-                    }
+                let docid = data.shopid + data.posterid + Math.floor(Math.random() * 99999);
+                console.log(docid);
+                await db.collection('reviews').doc(docid).set({
+                    text: data.text,
+                    posterid: data.posterid,
+                    postername: data.postername,
+                    posterimg: data.posterimg,
+                    posteremail: data.posteremail,
+                    shopname: data.shopname,
+                    shopid: data.shopid,
+                    shopdocid:shopdata.docs[0].id,
+                    time: time,
+                    fulltime: admin.firestore.FieldValue.serverTimestamp()
+
                 }).then(() => {
                     res.json({
                         status: 'success',
@@ -484,9 +531,9 @@ app.post('/add/review', async (req, res) => {
 //get reviews messages
 app.get('/get/review/:id', async (req, res) => {
     let { id } = req.params;
-    let data = await db.collection('reviews').doc(id).get();
-    if (data.exists) {
-        let reviewdata = data.data();
+    let data = await db.collection('reviews').where('shopdocid','==',id).get();
+    if (!data.empty) {
+        let reviewdata = data.docs[0].data();
         res.json({
             status: 'success',
             data: reviewdata
@@ -569,6 +616,162 @@ app.get('/miniapp/:id', async (req, res) => {
         res.json({
             status: 'fail',
             text: 'No ID found to get miniapp data.'
+        })
+    }
+})
+
+//send request
+app.post('/request/shop', async (req, res) => {
+    let data = req.body;
+    if (data.uid) {
+        await db.collection('requests').doc(data.uid).set({
+            shopname: data.shopname,
+            shoptype: data.shoptype,
+            shopdes: data.shopdes,
+            city: data.city,
+            ownername: data.username,
+            ownerid: data.uid,
+            ownerimg: data.profile,
+            owneremail: data.email,
+            status: 'none',
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+            res.json({
+                status: 'success',
+                text: 'Your request was successfully submitted.'
+            })
+        })
+    } else {
+        res.json({
+            status: 'fail',
+            text: 'Authentication required!'
+        })
+    }
+})
+
+//get all requests
+app.get('/all/requests', async (req, res) => {
+    let r = await db.collection('requests')
+        .orderBy('time', 'desc')
+        .get();
+    if (r.empty) {
+        res.json({
+            status: 'fail',
+            text: 'Error to get requests'
+        })
+    } else {
+        const data = [];
+        r.forEach(doc => {
+            const docData = doc.data();
+            if (docData.createdAt && docData.createdAt instanceof admin.firestore.Timestamp) {
+                docData.createdAt = docData.createdAt.toDate();
+            }
+            data.push({ id: doc.id, ...docData })
+        });
+        res.json({
+            status: 'success',
+            data: data
+        })
+    }
+})
+
+app.get('/request/:id', async (req, res) => {
+    let { id } = req.params;
+    if (id) {
+        let re = await db.collection('requests').doc(id).get();
+        if (re.exists) {
+            let da = re.data();
+            res.json({
+                status: 'success',
+                data: da
+            })
+        } else {
+            res.json({
+                status: 'fail',
+                text: 'No request with this user ID!'
+            })
+        }
+    } else {
+        res.json({
+            status: 'fail',
+            text: 'User ID required!'
+        })
+    }
+})
+
+//approve shop
+app.post('/approve/shop', async (req, res) => {
+    let data = req.body;
+    let g = await db.collection('requests').doc(data.ownerid).get();
+    if (g.exists) {
+        let reqdata = g.data()
+        if (reqdata.owneremail === data.owneremail && reqdata.ownerid === data.ownerid) {
+            await db.collection('requests').doc(data.ownerid).update({
+                status: 'approved'
+            }).then(async () => {
+                await db.collection('shop').doc(reqdata.ownerid).set({
+                    ownername: reqdata.ownername,
+                    email: reqdata.owneremail,
+                    ownerid: reqdata.ownerid,
+                    ownerimg: reqdata.ownerimg,
+                    shopname: reqdata.shopname,
+                    shoptype: reqdata.shoptype,
+                    shopdes: reqdata.shopdes,
+                    status: 'active',
+                    contact: {
+                        phone: '09*****'
+                    },
+                    city: reqdata.city,
+                    verify: 'Passed',
+                    shopid: reqdata.ownerid,
+                    logo: 'https://img.freepik.com/free-vector/shop-with-sign-we-are-open_23-2148547718.jpg?semt=ais_hybrid&w=740',
+                    background: 'https://img.freepik.com/free-vector/hand-drawn-w-colours-illustration_23-2149834200.jpg?semt=ais_hybrid&w=740'
+                }).then(() => {
+                    res.json({
+                        status: 'success',
+                        text: 'Shop request was approved.'
+                    })
+                })
+            })
+        } else {
+            res.json({
+                status: 'fail',
+                text: 'No permission to approve!'
+            })
+        }
+    } else {
+        res.json({
+            status: 'fail',
+            text: 'Error to approve shop requests'
+        })
+    }
+})
+
+//decline shop
+app.post('/decline/shop', async (req, res) => {
+    let data = req.body;
+    let g = await db.collection('requests').doc(data.ownerid).get();
+    if (g.exists) {
+        let reqdata = g.data()
+        if (reqdata.owneremail === data.owneremail && reqdata.ownerid === data.ownerid) {
+            await db.collection('requests').doc(data.ownerid).update({
+                status: 'declined'
+            }).then(async () => {
+                res.json({
+                    status: 'success',
+                    text: 'Shop request was declined.'
+                })
+            })
+        } else {
+            res.json({
+                status: 'fail',
+                text: 'No permission to approve!'
+            })
+        }
+    } else {
+        res.json({
+            status: 'fail',
+            text: 'Error to approve shop requests'
         })
     }
 })
